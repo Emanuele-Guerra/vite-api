319cd4d6916d7f86c485af2333d730757f62cfc9
Emanuele-Guerra <emaguerra1993@hotmail.it>
2023-11-30 17:18:55 +0100

    push

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script>
+  import AppCard from "./components/AppCard.vue"
+  
+  import axios from 'axios';
+  
+  
+  export default{
+    components: {
+      
+      AppCard
+
+    },
+    data() {
+        return{
+          pubs: []
+        }
+      },
+  
+  methods: {
+    getBeer() {
+
+      axios.get('https://api.openbrewerydb.org/v1/breweries?by_country=austria&per_page=10').then(risultato =>{
+        this.pubs.push(...risultato.data)
+        console.log(this.pubs)
+          
+          
+      })
+    }
+  },
+  mounted(){
+    this.getBeer();
+  }
+};
+  
+  
+ 
+</script>
+
+<template>
+    <AppCard :arrayGenerato="pubs" />
+</template>
+
+<style scoped>
+</style>
