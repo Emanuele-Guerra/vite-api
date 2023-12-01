47b30582dee8a10b9a26dd87adda26d30cfedce7
Emanuele-Guerra <emaguerra1993@hotmail.it>
2023-12-01 16:46:48 +0100

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <script>
   import AppCard from "./components/AppCard.vue"
-  
+  import { store } from "../store.js"
+  import AppSearchComp from "./components/AppSearchComp.vue";
   import axios from 'axios';
   
   
   export default{
     components: {
       
-      AppCard
+      AppCard,
+      AppSearchComp
 
     },
     data() {
         return{
-          pubs: []
+          pubs: [],
+          store
         }
       },
   
@@ -25,6 +28,14 @@
           
           
       })
+    },
+    getInfos(){
+      this.pubs = []
+      axios.get(`https://api.openbrewerydb.org/v1/breweries?by_name=${store.searchString}&per_page=10`).then(risultato => {
+        this.pubs.push(...risultato.data)
+      })
+      
+
     }
   },
   mounted(){
@@ -37,8 +48,10 @@
 </script>
 
 <template>
+    <AppSearchComp @searching="getInfos" />
     <AppCard :arrayGenerato="pubs" />
 </template>
 
 <style scoped>
+  
 </style>
